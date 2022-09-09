be21277408fcc70e41b33a6db1a299bf2598dafb
Felipe Herrera <felipeherrerapinzon@gmail.com>
2022-09-09 10:50:05 -0500

    actualizadooo

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,8 @@
   
 <nav class="navbar navbar-expand navbar-light bg-light">
    <div class="navbar navbar-nav">
-              
+
+   <router-link to="/" class="nav-link"> HOME</router-link>           
    <router-link to="/crear" class="nav-link"> CREAR</router-link>
    <router-link to="/listar" class="nav-link">EMPLEADOS</router-link>
                
